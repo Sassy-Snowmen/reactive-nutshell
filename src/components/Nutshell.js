43db9f8eef32d06396109d41520f4fd8d3b21b6f
Michelle Johnson <bob1234girl@gmail.com>
2019-12-04 13:50:14 -0600

    sessionStorage

--- src/components/Nutshell.js
+++ src/components/Nutshell.js
@@ -8,7 +8,6 @@ class Nutshell extends Component {
   render() {
     return (
       <React.Fragment>
-        {localStorage.setItem("credentials", 2)}
         <NavBar />
         <ApplicationViews />
       </React.Fragment>
