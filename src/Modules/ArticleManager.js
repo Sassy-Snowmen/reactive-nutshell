cfabf72e0810f46e08bd57a6869618f97044f82e
Michelle Johnson <bob1234girl@gmail.com>
2019-12-04 10:43:18 -0600

    sorted articles in descending order

--- src/Modules/ArticleManager.js
+++ src/Modules/ArticleManager.js
@@ -5,7 +5,7 @@ export default {
     return fetch(`${remoteURL}/articles/${id}`).then(result => result.json())
   },
   getAll() {
-    return fetch(`${remoteURL}/articles`).then(result => result.json())
+    return fetch(`${remoteURL}/articles?_sort=timestamp&_order=desc`).then(result => result.json())
   },
   delete(id) {
     return fetch(`http://localhost:5002/articles/${id}`, {
